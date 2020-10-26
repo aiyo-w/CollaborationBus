a1d4b47140efe803b2a1a9b94e068f0b4c283f8d
aiyo_w <aiyo_w@outlook.com>
2020-10-26 20:00:33 +0800

    modify 使用UDP发送接收消息

---
+++ src/CollaborationBus/Net/Connector.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace CollaborationBus
+{
+    /// <summary>
+    /// 连接两台电脑的类
+    /// </summary>
+    public class Connector
+    {
+        private UdpClient _udpClient;
+        private NetworkStream _networkStream;
+        private byte[] _dataBuffer;
+
+        private string _address;
+        private int _port;
+
+        public event EventHandler<string> RecevieMessage;
+
+        public Connector(string address, int port)
+        {
+            _dataBuffer = new byte[2048];
+            _address = address;
+            _port = port;
+
+            _udpClient = new UdpClient(new IPEndPoint(IPAddress.Any,9898));
+            _udpClient.JoinMulticastGroup(IPAddress.Parse("239.0.0.1"));
+
+            Thread thread = new Thread(ReciveMessage);
+            thread.Start();
+        }
+
+        public void SendMessage(string message)
+        {
+            byte[] messageBuffer = Encoding.UTF8.GetBytes(message);
+            _udpClient.Send(messageBuffer, messageBuffer.Length);
+            Debug.WriteLine("send {0}", message);
+        }
+
+        public void ReciveMessage()
+        {
+            while (true)
+            {
+                IPEndPoint iep = new IPEndPoint(IPAddress.Any,0);
+                byte[] messageBuffer = _udpClient.Receive(ref iep);
+
+                string recevieMessage = Encoding.UTF8.GetString(messageBuffer);
+
+                RecevieMessage?.Invoke(this,recevieMessage);
+            }
+        }
+
+        //发送“Hello”消息
+    }
+}
--- src/CollaborationDemo/App.xaml.cs
+++ src/CollaborationDemo/App.xaml.cs
@@ -18,13 +18,11 @@ namespace CollaborationDemo
     {
         protected override void OnStartup(StartupEventArgs e)
         {
-            Conector conector = new Conector("192.168.8.143", 9898);
-            //conector.Connect();
-            //conector.SendMessage("Hello");
-            var s = conector.ReciveMessage();
+            Connector connector = new Connector("192.168.8.143", 9898);
+            connector.SendMessage("Hello");
 
-            var msg = Encoding.UTF8.GetString(s);
-            Debug.WriteLine("receive {0}", msg);
+            //var msg = Encoding.UTF8.GetString(s);
+            //Debug.WriteLine("receive {0}", msg);
 
             //var mainWindow = new MainWindow();
             //mainWindow.Show();
