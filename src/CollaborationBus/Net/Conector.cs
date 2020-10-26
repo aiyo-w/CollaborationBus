e36471c5857fe469135f1fece110e62ab1673533
standevil <standevil@gmail.com>
2020-10-26 15:41:39 +0800

    add connector

--- src/CollaborationBus/Net/Conector.cs
+++ src/CollaborationBus/Net/Conector.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Net;
 using System.Net.Sockets;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace CollaborationBus
 {
@@ -12,15 +14,46 @@ namespace CollaborationBus
     public class Conector
     {
         private TcpClient _tcpClient;
+        private TcpListener _tcpListener;
         private NetworkStream _networkStream;
         private byte[] _dataBuffer;
+
+        private string _address;
+        private int _port;
 
         public Conector(string address, int port)
         {
             _dataBuffer = new byte[2048];
-            _tcpClient = new TcpClient(new IPEndPoint(IPAddress.Parse(address), port));
+            _address = address;
+            _port = port;
+            //_tcpClient = new TcpClient(address, port);
+            _tcpListener = new TcpListener(IPAddress.Parse("0.0.0.0"), port);
+
+
+            _tcpListener.Start();
+            _tcpClient = _tcpListener.AcceptTcpClient();
             _networkStream = _tcpClient.GetStream();
+
+            //Task.Run(() =>
+            //{
+            //});
         }
+
+        public void Connect()
+        {
+            try
+            {
+                var tcpClient = new TcpClient();
+                tcpClient.Connect(_address, _port);
+                _tcpClient = tcpClient;
+                _networkStream = _tcpClient.GetStream();
+            }
+            catch (Exception ex)
+            {
+
+            }
+        }
+
 
         // TODO:建立Soket连接
         public void SendMessage(string message)
@@ -28,6 +61,7 @@ namespace CollaborationBus
             // TODO:发送消息
             //_tcpClient.Client.Send(Encoding.UTF8.GetBytes(message));
             _networkStream.Write(Encoding.UTF8.GetBytes(message));
+            Debug.WriteLine("send {0}", message);
         }
 
         public byte[] ReciveMessage()
--- src/CollaborationDemo/App.xaml.cs
+++ src/CollaborationDemo/App.xaml.cs
@@ -2,7 +2,9 @@
 using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
+using System.Diagnostics;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
 using CollaborationBus;
@@ -16,8 +18,13 @@ namespace CollaborationDemo
     {
         protected override void OnStartup(StartupEventArgs e)
         {
-            Conector conector = new Conector("192.168.8.187", 9898);
-            conector.SendMessage("Hello");
+            Conector conector = new Conector("192.168.8.143", 9898);
+            //conector.Connect();
+            //conector.SendMessage("Hello");
+            var s = conector.ReciveMessage();
+
+            var msg = Encoding.UTF8.GetString(s);
+            Debug.WriteLine("receive {0}", msg);
 
             //var mainWindow = new MainWindow();
             //mainWindow.Show();
